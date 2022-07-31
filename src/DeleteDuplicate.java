6c3313009d57752b2758f7d6831dcbaf32f52ece
sung-jun park <kick07230723@gmail.com>
2022-07-31 12:42:07 +0900

    (스택/큐) 같은 숫자는 싫어
    https://school.programmers.co.kr/learn/courses/30/lessons/12906

---
+++ src/DeleteDuplicate.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class DeleteDuplicate {
+
+    public static void main(String[] args) {
+        int[] arr = {1,1,3,3,0,1,1};
+        solution(arr);
+    }
+
+    public static int[] solution(int []arr) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            if(i == 0 || arr[i] != arr[i-1]) {
+                list.add(arr[i]);
+            }
+        }
+        // Stream을 이용한 List -> int[]
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+}
