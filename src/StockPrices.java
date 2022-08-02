76f285dacd0e1d101ea3029a12890de004c926a3
sung-jun park <kick07230723@gmail.com>
2022-08-02 22:08:42 +0900

    (스택/큐) 주식 가격
    https://school.programmers.co.kr/learn/courses/30/lessons/42584

---
+++ src/StockPrices.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class StockPrices {
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,2,3};
+        solution(arr);
+    }
+
+    public static int[] solution(int[] prices) {
+        List<Integer> secondsArr = new ArrayList<>();
+        for (int i = 0; i < prices.length; i++) {
+            int seconds = 0;
+            for (int j = i; j < prices.length; j++) {
+                if (prices[i] > prices[j] || j == prices.length-1) {
+                    secondsArr.add(seconds);
+                    break;
+                }else{
+                    seconds ++;
+                }
+            }
+        }
+        return secondsArr.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+}
