ee12f44564d2e1e52e46e495a3407b3107820a7e
sung-jun park <kick07230723@gmail.com>
2022-08-04 23:36:52 +0900

    (해시)폰켓못

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,28 +1,32 @@
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 
-public class Camouflage {
+public class Pokemon {
 
-    public static void main(String[] args) {
-        String[][] clothes = {{"yellowhat", "headgear"}, {"bluesunglasses", "eyewear"}, {"green_turban", "headgear"}};
-        System.out.println("####  Result : "+solution(clothes));
+    public static void main(String[] args){
+        int[] nums = {3,3,3,2,2,4};
+        int result = solution(nums);
+        System.out.println("### Result : "+ result);
     }
 
-    public static int solution(String[][] clothes) {
-        int answer = 1;
-        HashMap<String, Integer> clothesMap = new HashMap<>();
-        // Hash로 저장
-        for (int i = 0; i < clothes.length; i++) {
-            clothesMap.put(clothes[i][1], clothesMap.get(clothes[i][1])==null ? 2:clothesMap.get(clothes[i][1])+1);
+
+    public static int solution(int[] nums) {
+        int answer = 0;
+        // arr -> hash : 중복제거 (hashMap)
+//        HashMap<String, Integer> tempMap = new HashMap<>();
+//        for (int num : nums) {
+//            tempMap.put(String.valueOf(num), 0);
+//        }
+
+        // arr -> hash : 중복제거 (hashSet)
+        HashSet<Integer> tempSet = new HashSet<>();
+        for(int i =0; i<nums.length;i++) {
+            tempSet.add(nums[i]);
         }
 
-        // Hash를 이용해 key(카테고리), value(count++) 해서 count 계산
-        for (String category :clothesMap.keySet()) {
-            answer *= clothesMap.get(category);
-        }
-
-        // count들 끼리 곱 한후 -1 (모두 0을 고른 경우)
-        return answer-1;
+        // map 크기 중 arr/2 크기 만큼 해서 최댓값 구하기
+        int kindCount = tempSet.size();
+        int selectCount = nums.length/2;
+        return Math.min(kindCount, selectCount);
     }
 }
