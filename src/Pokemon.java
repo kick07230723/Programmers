22dc1057c962d1e2832e0c33bb5d799437927b94
sung-jun park <kick07230723@gmail.com>
2022-08-04 23:30:08 +0900

    (해시)폰켓못
    https://school.programmers.co.kr/learn/courses/30/lessons/1845

---
+++ src/Pokemon.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class Camouflage {
+
+    public static void main(String[] args) {
+        String[][] clothes = {{"yellowhat", "headgear"}, {"bluesunglasses", "eyewear"}, {"green_turban", "headgear"}};
+        System.out.println("####  Result : "+solution(clothes));
+    }
+
+    public static int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String, Integer> clothesMap = new HashMap<>();
+        // Hash로 저장
+        for (int i = 0; i < clothes.length; i++) {
+            clothesMap.put(clothes[i][1], clothesMap.get(clothes[i][1])==null ? 2:clothesMap.get(clothes[i][1])+1);
+        }
+
+        // Hash를 이용해 key(카테고리), value(count++) 해서 count 계산
+        for (String category :clothesMap.keySet()) {
+            answer *= clothesMap.get(category);
+        }
+
+        // count들 끼리 곱 한후 -1 (모두 0을 고른 경우)
+        return answer-1;
+    }
+}
