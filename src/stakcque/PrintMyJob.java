db3e8e1003a0c8888033d0f1b01c7a182562f7d1
sung-jun park <kick07230723@gmail.com>
2022-08-06 13:13:06 +0900

    (스택/큐) 프린터
    https://school.programmers.co.kr/learn/courses/30/lessons/42587

--- src/stakcque/PrintMyJob.java
+++ src/stakcque/PrintMyJob.java
@@ -1,24 +1,47 @@
 package stakcque;
 
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class PrintMyJob {
 
     public static void main(String[] args) {
-        int[] priorities = {2,1,3,2};
-        int location = 2;
+        int[] priorities = {1, 1, 9, 1, 1, 1};
+        int location = 0;
         solution(priorities, location);
     }
 
     public static int solution(int[] priorities, int location) {
         int answer = 0;
-        for (int i = 0; i < priorities.length; i++) {
-            int bigNum = 0;
-            for (int j = i; j < priorities.length; j++) {
-//                priorities[i]
-            }
+        Integer biggestNum = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        ArrayList<Integer> sortedPriorities = new ArrayList();
+        for (int num: priorities) {
+            queue.add(num);
+            sortedPriorities.add(num);
         }
+        sortedPriorities.sort(Collections.reverseOrder());
+
+        while(true){
+            biggestNum = sortedPriorities.get(answer);
+            if (location == 0 && queue.peek() == biggestNum){
+                answer++;
+                break;
+            } else if ((Integer)queue.peek() < biggestNum){
+                queue.add(queue.poll());
+            } else {
+                queue.poll();
+                answer++;
+            }
+
+            if(location==0)
+                location = queue.size()-1;
+            else
+                location--;
+        }
+
         return answer;
     }
 
